f7b1ef2717bffe053eb605d9f181e2f7fa984a3a
Marc Cyr <marcacyr@gmail.com>
2015-07-26 11:31:00 -0700

    boilerplate code

---
+++ app/assets/javascripts/messages-boilerplate.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $('#message-submit').on('click', function() {
+    processMessageAndAddToList();
+  });
+
+  $('#message-input').keyup(function(event) {
+    if (event.keyCode !== 13) { return; }
+    processMessageAndAddToList();
+  });
+
+  function processMessageAndAddToList() {
+    var messageAndContainer = getMessageAndContainer();
+    var message = messageAndContainer.message;
+    var messageInput = messageAndContainer.messageInput;
+    if (!message) { return; }
+    appendMessageAndClearInput(message, messageInput);
+  }
+
+  function appendMessageAndClearInput(message, messageInput) {
+    var messagesListContainer = $('#messages-list');
+    messagesListContainer.append('<li>' + message + '</li>');
+    messageInput.val('');
+  }
+
+  function getMessageAndContainer() {
+    var messageInput = $('#message-input');
+    var message = messageInput.val();
+    return {
+      messageInput: messageInput,
+      message: message
+    };
+  }
+});
