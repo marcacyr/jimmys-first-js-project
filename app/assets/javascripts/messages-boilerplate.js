1e2699bc78fd2225b4c115d22be4a150426fb479
James Schwarzenberger <jamesschwarzenberger@Jamess-MacBook-Air.local>
2015-07-26 13:08:43 -0700

    comment marc code

--- app/assets/javascripts/messages-boilerplate.js
+++ app/assets/javascripts/messages-boilerplate.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-  $('#message-submit').on('click', function() {
-    processMessageAndAddToList();
-  });
+// $(document).ready(function() {
+//   $('#message-submit').on('click', function() {
+//     processMessageAndAddToList();
+//   });
 
-  $('#message-input').keyup(function(event) {
-    if (event.keyCode !== 13) { return; }
-    processMessageAndAddToList();
-  });
+//   $('#message-input').keyup(function(event) {
+//     if (event.keyCode !== 13) { return; }
+//     processMessageAndAddToList();
+//   });
 
-  function processMessageAndAddToList() {
-    var messageInput = $('#message-input');
-    var message = messageInput.val();
-    if (!message) { return; }
-    var messagesList = $('#messages-list');
-    messagesList.append('<li>' + message + '<span>x</span></li>');
-    messageInput.val('');
-  }
-});
+//   function processMessageAndAddToList() {
+//     var messageInput = $('#message-input');
+//     var message = messageInput.val();
+//     if (!message) { return; }
+//     var messagesList = $('#messages-list');
+//     messagesList.append('<li>' + message + '<span>x</span></li>');
+//     messageInput.val('');
+//   }
+// });
 
